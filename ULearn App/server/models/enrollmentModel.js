d215d9e7820e2441f92718ff06f02e7582556bde
ElaineLi0320 <yinili0320@gmail.com>
2025-04-06 14:46:52 -0700

    update schema to allow string ID

--- ULearn App/server/models/enrollmentModel.js
+++ ULearn App/server/models/enrollmentModel.js
@@ -1,25 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const enrollmentSchema = new mongoose.Schema({
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      auto: true
-    },
-    student_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    course_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Course',
-      required: true
-    },
-    enrolled_at: {
-      type: Date,
-      default: Date.now
-    }
-  });
-  
-  const Enrollment = mongoose.model('Enrollment', enrollmentSchema);
-  export default Enrollment;
+  _id: {
+    type: String,
+    required: true,
+  },
+  student_id: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+  course_id: {
+    type: String,
+    ref: "Course",
+    required: true,
+  },
+  enrolled_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Enrollment = mongoose.model("Enrollment", enrollmentSchema);
+export default Enrollment;
